7134278f61f3748c00cb045202ad06fc63ff2c2c
Joonwan Kim <joonwk@gmail.com>
2023-07-06 22:07:34 +0900

    border rounded

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({
         <section className="pb-4 mt-3">
             <div className='bg-white'>
                 <section className='w-100 d-flex justify-content-center align-items-center'>
-                    <div className="border border-round border-info p-3">
+                    <div className="border rounded border-info p-3">
                         <div className={styles.main}>
                             {children}
                         </div>
